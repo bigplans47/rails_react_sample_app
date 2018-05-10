3ea45563bcd5e98865d83bf2834883c8a63ef3cd
Dan Kiss <flowfast47@gmail.com>
2018-05-10 16:28:29 -0700

    add create controller to be able to post new quotes and authors

--- app/controllers/api/quotes_controller.rb
+++ app/controllers/api/quotes_controller.rb
@@ -1,5 +1,16 @@
 class Api::QuotesController < ApplicationController
+  skip_before_action :verify_authenticity_token  
   def show
     @quote = Quote.find(params[:id])
+  end
+
+  def create
+   @single_quote = Quote.create!(quote_params)
+   # json_response(@destination, :created)
+  end
+
+  private
+  def quote_params
+    params.permit(:text, :author)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   # get 'pages/home' #this url is  to left is removed
   root to: "pages#home"
   namespace :api, defaults: {format: :json} do
-    resources :quotes, only: [ :show ]
+    resources :quotes, only: [ :show, :create ]
   end
 end
