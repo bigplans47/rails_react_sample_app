a550f5e9300efc3a38508ce50950fef732dc76a9
Dan Kiss <flowfast47@gmail.com>
2018-05-10 13:53:16 -0700

    setup show controller and way to output api data as json using jbuilder

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get 'pages/home'
-  # check line above
+  # get 'pages/home' #this url is  to left is removed
   root to: "pages#home"
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api, defaults: {format: :json} do
+    resources :quotes, only: [ :show ]
+  end
 end
